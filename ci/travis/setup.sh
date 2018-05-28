b567b1c5e450fe918cb39b04c0619835d1d40dcf
Martin Larralde <martin.larralde@ens-cachan.fr>
2018-05-28 18:53:21 +0200

    Use extern script to setup Travis-CI

---
+++ ci/travis/setup.sh
@@ -0,0 +1,68 @@
+#!/bin/sh
+
+set -e +x
+
+# Find the installed version of a binary, if any
+_installed() {
+    VERSION=$($@ --version 2>/dev/null || echo "$@ none")
+    echo $VERSION | rev | cut -d' ' -f1 | rev
+}
+
+# Find the latest available version of a binary on `crates.io`
+_latest() {
+    VERSION=$(cargo search -q "$@" | grep "$@" | cut -f2 -d"\"")
+    echo $VERSION
+}
+
+
+### Setup sccache ##############################################################
+
+echo -n "Fetching latest available 'sccache' version... "
+INSTALLED=$(_installed sccache)
+LATEST=$(_latest sccache)
+echo "${LATEST} (installed: ${INSTALLED})"
+
+if [ "$INSTALLED" = "$LATEST" ]; then
+  echo "Using cached 'sccache'"
+else
+  echo "Installing latest 'sccache' from mozilla/sccache"
+  URL="https://github.com/mozilla/sccache/releases/download/${LATEST}/sccache-${LATEST}-x86_64-unknown-linux-musl.tar.gz"
+  curl -SsL $URL | tar xzv -C /tmp
+  mv /tmp/sccache-${LATEST}-x86_64-unknown-linux-musl/sccache $HOME/.cargo/bin/sccache
+fi
+
+mkdir -p $SCCACHE_DIR
+
+
+### Setup cargo-make ###########################################################
+
+echo -n "Fetching latest available 'cargo-make' version..."
+INSTALLED=$(_installed cargo make)
+LATEST=$(_latest cargo-make)
+echo "${LATEST} (installed: ${INSTALLED})"
+
+if [ "$INSTALLED" = "$LATEST" ]; then
+  echo "Using cached 'cargo-make'"
+else
+  echo "Installing latest 'cargo-make' from source"
+  cargo install --debug -f cargo-make
+fi
+
+mkdir -p $SCCACHE_DIR
+
+
+### Setup vdpm #################################################################
+
+echo "Fetching latest Vita SDK..."
+
+mkdir -p "$VITASDK/src/vdpm"
+cd "$VITASDK/src/vdpm"
+
+if [ -d ".git" ]; then
+  git pull
+else
+  git clone https://github.com/vitasdk/vdpm .
+fi
+
+./bootstrap-vitasdk.sh
+./install.sh
